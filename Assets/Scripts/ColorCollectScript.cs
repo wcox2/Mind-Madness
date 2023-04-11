0bac914b55a7d89defb1caa4965b63ba520c279c
Patrick Leary <paleary@davidson.edu>
2023-04-11 10:20:22 -0400

    Lighting Changes and Worked on Patrick Scene

--- Assets/Scripts/ColorCollectScript.cs
+++ Assets/Scripts/ColorCollectScript.cs
@@ -7,9 +7,14 @@ public class ColorCollectScript : MonoBehaviour {
     public GameObject platform;
     private Collider playerCollider;
     private Collider colorCollider;
+    public Light playerLight;
+    public GameObject sprite;
+    private SpriteRenderer spriteRenderer;
     
 
     void Start() {
+        // playerLight = player.transform.GetChild(2).gameObject;
+        spriteRenderer = sprite.GetComponent<SpriteRenderer>();
         colorCollider = GetComponent<Collider>();
         playerCollider = player.GetComponent<Collider>();
     }
@@ -19,9 +24,12 @@ public class ColorCollectScript : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider col) {
-            if (col.tag == "Player") {
-                platform.SetActive(true);
-                Destroy(gameObject);
-            }
+        playerLight.color = Color.red;
+        // Color newColor = new Color(1.0f, 0.5f, 0.0f, 1.0f);
+        spriteRenderer.color = new Color(1f, 0.45f, 0.45f, 1f);
+        if (col.tag == "Player") {
+            platform.SetActive(true);
+            Destroy(gameObject);
         }
+    }
 }
