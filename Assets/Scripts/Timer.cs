4f63d0222b454dc7986fdbb7cc7caf007ed9eb66
Will Cox <wicox@davidson.edu>
2023-04-25 09:49:21 -0400

    Updated Timer Format

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -13,17 +13,35 @@ public class Timer : MonoBehaviour
     [Header("Timer Settings")]
     public float currentTime;
     public bool countDown;
+    public float minutes = 0f;
 
-    [SerializeField] Text countdownTimer;
+    [Header("Format Settings")]
+    public bool hasFormat;
+    public TimerFormats format;
+    private Dictionary<TimerFormats, string> timeFormats = new Dictionary<TimerFormats, string>();
 
     void Start()
     {
-        
+        timeFormats.Add(TimerFormats.Whole, "0");
+        timeFormats.Add(TimerFormats.TenthDecimal, "0.0");
+        timeFormats.Add(TimerFormats.HundredthsDecimal, "0.00");
+
     }
 
     void Update()
     {
         currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
-        timerText.text = Math.Round(currentTime).ToString();
+        SetTimerText();
+    }
+
+    public void SetTimerText()
+    {
+        timerText.text = hasFormat ? currentTime.ToString(timeFormats[format]) : currentTime.ToString();
+    }
+    public enum TimerFormats
+    {
+        Whole,
+        TenthDecimal,
+        HundredthsDecimal
     }
 }
