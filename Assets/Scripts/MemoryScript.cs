0005804e66139056275708ad90773630290938bd
Patrick Leary <paleary@davidson.edu>
2023-04-27 10:25:44 -0400

    memory

--- Assets/Scripts/MemoryScript.cs
+++ Assets/Scripts/MemoryScript.cs
@@ -5,21 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MemoryScript : MonoBehaviour
 {
+    public string nextScene;
     public float amplitude = 1f; // Set the amplitude of the object's movement
     public float speed = 1f; // Set the speed of the object's movement
     public float rotationSpeed = 1f; // Set the speed of the object's rotation
     private Vector3 startPos; // Store the object's initial position
-    public string sceneName;
     
 
     void Start()
     {
         startPos = transform.position; // Set the object's initial position
-
-        
-        Scene scene  = SceneManager.GetActiveScene();
-        sceneName = scene.name;
-        Debug.Log(sceneName);
 
     }
 
@@ -35,13 +30,6 @@ public class MemoryScript : MonoBehaviour
     }
 
     void OnTriggerEnter() {
-        
-        /*if(sceneName == "TutorialLevel"){
-            SceneManager.LoadScene("HomeScreen");       
-        }
-        else if(sceneName == "Level 2"){
-            SceneManager.LoadScene("Patrick Scene");
-        }*/
-        
+        SceneManager.LoadScene(nextScene);
     }
 }
