cd03b3b636e074c051d85c36eac0ef38c1909849
Aina Solis <aisolis@davidson.edu>
2023-04-30 15:44:39 -0400

    Revert "Squashed commit of the following:"
    This reverts commit c72406d41582934ffe2faf98ec2a2eaef1516366.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,62 +19,59 @@ public class PlayerMovement : MonoBehaviour {
     private bool isFrozen = false;
     private float hInput;
     public int maxJumps = 2;
-    private int currentJumps = 2;
+    public int currentJumps = 2;
     private Rigidbody _rb;
     enum playerAction {jump};
-    private bool isGrounded = true;
+    public bool isGrounded = true;
     private bool isFacingRight = true;
     public GameObject mySpawnPoint;
-
+    //public Animator animator;
     public AnimationScript animationScript;
+    private int orbsCollected = 0;
+    public GameController GameController;
 
     // Start is called before the first frame update
     void Start() {
         sprite = this.transform.GetChild(0).gameObject;
         _rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -25f, 0);
     }
 
     // Update is called once per frame
     void Update() {
 
+        // if (!GameController.isPaused) {
+            // Jump
+            if(Input.GetKeyDown(KeyCode.W) && (isFrozen == false) && (isDashing == false) && (currentJumps > 0)) {
+                _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+                _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+                currentJumps--;
+            }
 
-        // Jump
-        if(Input.GetKeyDown(KeyCode.W) && (isFrozen == false) && (isDashing == false) && (currentJumps > 0)) {
-            _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
-            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
-            currentJumps--;
-            Debug.Log(currentJumps);
-            animationScript.UpdateIsJumping(true);
+            // Dash
+            if(Input.GetKeyDown(KeyCode.Space) && (isFrozen == false) && (canDash)) { 
+                StartCoroutine(dash());
+            }
 
-        }
+            // Downslam
+            if(Input.GetKeyDown(KeyCode.S) && (isFrozen == false) && (isGrounded == false)) {
+                StartCoroutine(freezePlayerTimer(slamFreezeTime));
+            }
 
-        // Dash
-        if (Input.GetKeyDown(KeyCode.Space) && (isFrozen == false) && (canDash)) { 
-            StartCoroutine(dash());
-        }
+            // Player Orientation Change
+            if (Input.GetKeyDown(KeyCode.D) && (isFacingRight == false)) {
+                isFacingRight = true;
+                sprite.transform.Rotate(0, 180, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.A) && (isFacingRight)) {
+                isFacingRight = false;
+                sprite.transform.Rotate(0, 180, 0);
+            }
 
-        // Downslam
-        if(Input.GetKeyDown(KeyCode.S) && (isFrozen == false) && (isGrounded == false)) {
-            StartCoroutine(freezePlayerTimer(slamFreezeTime));
-            animationScript.UpdateIsSlamming(true);
-            animationScript.UpdateIsJumping(false);
-        }
-
-        // Player Orientation Change
-        if (Input.GetKeyDown(KeyCode.D) && (isFacingRight == false)) {
-            isFacingRight = true;
-            sprite.transform.Rotate(0, 180, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.A) && (isFacingRight)) {
-            isFacingRight = false;
-            sprite.transform.Rotate(0, 180, 0);
-        }
-
-        hInput = Input.GetAxis("Horizontal") * movementSpeed;
-
-        animationScript.UpdateSpeed(hInput);
+            hInput = Input.GetAxis("Horizontal") * movementSpeed;
+            //animationScript.UpdateSpeed(hInput);
+        // }
     }
-
 
     void FixedUpdate() {
         if ((isFrozen == false) && (isDashing == false)) {
@@ -83,36 +80,61 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void OnCollisionEnter(Collision collision) {
-        StartCoroutine(waitForSlam());
-        if (collision.gameObject.CompareTag("Ground")) {
-            currentJumps = maxJumps;
-            isGrounded = true;
-
-            animationScript.UpdateIsJumping(false);
-            animationScript.UpdateIsSlamming(false);
-
+        Vector3 collisionNormal = collision.contacts[0].normal;
+        if (isSlamming) {
+            _rb.velocity = new Vector3(0f, -25f, 0f);
+        }
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("BreakableGround")) {
+            collisionNormal = collision.contacts[0].normal;
+            // Check if the collision normal is pointing up (i.e. the bottom of the player is touching the ground)
+            if (collisionNormal.y > 0.5f) {
+                currentJumps = maxJumps;
+                isGrounded = true;
+            }
+        }
+        if (!collision.gameObject.CompareTag("BreakableGround")) {
+            collisionNormal = collision.contacts[0].normal;
+            if (collisionNormal.y > 0.5f) {
+                isSlamming = false;
+                unfreezePlayer();
+            }
         }
 
         if(collision.gameObject.tag == "FloorLimit"){
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            gameObject.transform.position = mySpawnPoint.transform.position;
+        }
+
+      
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "ColorOrb"){
+            mySpawnPoint.transform.position = sprite.transform.position;
+        }
+        if(other.gameObject.tag == "Spikes"){
             gameObject.transform.position = mySpawnPoint.transform.position;
         }
     }
 
+
     void OnCollisionExit(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("BreakableGround")) {
             isGrounded = false;
-            
         }
     }
     
     private IEnumerator freezePlayerTimer(float time) {
         isSlamming = true;
         freezePlayer();
+        _rb.isKinematic = true;
         yield return new WaitForSecondsRealtime(time);
         _rb.isKinematic = false;
         _rb.velocity = new Vector3(0, 0, 0);
-        _rb.AddForce(Vector3.down * slamForce, ForceMode.Impulse);
-        isFrozen = false;
+        // _rb.AddForce(Vector3.down * slamForce, ForceMode.Impulse);
+        _rb.velocity = new Vector3(0f, -25f, 0f);
     }
 
     private IEnumerator waitForSlam() {
@@ -135,17 +157,24 @@ public class PlayerMovement : MonoBehaviour {
         yield return new WaitForSecondsRealtime(dashTime/3);
         _rb.drag = 5;
         yield return new WaitForSecondsRealtime(dashTime/3);
-        _rb.drag = 30;
-        yield return new WaitForSecondsRealtime(dashTime/3);
-        _rb.drag = 1;
-        isDashing = false;
+        _rb.drag = 2;
+        _rb.drag = 15;
+        yield return new WaitForSecondsRealtime(0.0001f);
+        _rb.drag = 0;
         _rb.useGravity = true;
+        yield return new WaitForSecondsRealtime(dashTime/6);
+        _rb.velocity = new Vector3(0f, 0f, 0f);
+        isDashing = false;
         yield return new WaitForSecondsRealtime(dashCooldown);
         canDash = true;
     }
 
     void freezePlayer() {
         isFrozen = true;
-        _rb.isKinematic = true;
+        _rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+    }
+    void unfreezePlayer() {
+        isFrozen = false;
+        _rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 }
