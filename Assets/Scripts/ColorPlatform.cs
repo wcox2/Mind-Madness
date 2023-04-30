c72406d41582934ffe2faf98ec2a2eaef1516366
Aina Solis <aisolis@davidson.edu>
2023-04-30 15:40:52 -0400

    Squashed commit of the following:
    commit 736e6e5347cd39c7654f51ec288b2ba501741925
    Merge: 447e4b2 5988842
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Sun Apr 30 15:39:46 2023 -0400
    
        Merge branch 'main' into main_animation02
    
    commit 447e4b27c79200dec3fe9860f18f22c43c183a95
    Merge: fe12382 daf60f4
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Sun Apr 30 15:31:44 2023 -0400
    
        Merge branch 'main' into main_animation02
    
    commit fe123824c3ada23062db1342393121294156c776
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Sun Apr 30 15:30:01 2023 -0400
    
        Revert "Merge branch 'main' into main_animation02"
    
        This reverts commit 7cd0f683acf8123a6286af95307133c707d64440, reversing
        changes made to 7de415da9781f2c12743ef28d004d7a361556d93.
    
    commit 7cd0f683acf8123a6286af95307133c707d64440
    Merge: 7de415d 4472887
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Sun Apr 30 15:26:53 2023 -0400
    
        Merge branch 'main' into main_animation02
    
    commit 7de415da9781f2c12743ef28d004d7a361556d93
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Sun Apr 30 15:23:24 2023 -0400
    
        revert merge from main
    
    commit 9f25ef3be7ef4272c81394ca85f4d2e0540c15dc
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Sun Apr 30 14:36:19 2023 -0400
    
        commit changes of background
    
    commit 36663b2cf88fd407400ab83d85aafff22f5b6674
    Merge: 249010c 6e9fa88
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Tue Apr 25 10:12:55 2023 -0400
    
        Merge branch 'main' into animation
    
    commit 249010c210a507c90c364018d8394b49acd91a96
    Author: Aina Solis <aisolis@davidson.edu>
    Date:   Tue Apr 18 10:44:35 2023 -0400
    
        Revert "Update from Main Branch"
    
        This reverts commit ff86446704bbb949f2e8eb59c4e8b5dc1580d019, reversing
        changes made to 96eafe04d306dcea9ddfb6647b06ad98ff629d0d.

--- Assets/Scripts/ColorPlatform.cs
+++ Assets/Scripts/ColorPlatform.cs
@@ -22,26 +22,17 @@ public class ColorPlatform : MonoBehaviour {
     }
 
     void OnEnable() {
-        if (this.transform.childCount > 0) {
-            light = this.transform.GetChild(0).GetComponent<Light>();
-            light.intensity = 0;
-        }
+        light = this.transform.GetChild(0).GetComponent<Light>();
+        light.intensity = 0;
         StartCoroutine(FadeInObject());
     }
 
     public IEnumerator FadeInObject() {
-        Material[] materials = this.GetComponent<Renderer>().materials;
         for (float f = 0; f <= 1.001; f += fadeSpeed) {
-            Color color;
-            foreach (Material material in materials) {
-                color = material.color;
-                color.a = f;
-                material.color = color;
-            }
-            if (this.transform.childCount > 0) {
-                this.transform.GetChild(0).GetComponent<Light>().intensity = f;
-            }
-            // this.GetComponent<Renderer>().material.color = color;
+            Color color = this.GetComponent<Renderer>().material.color;
+            color.a = f;
+            light.intensity = f;
+            this.GetComponent<Renderer>().material.color = color;
             yield return new WaitForSecondsRealtime(fadeSpeed);
         }
     }
@@ -50,12 +41,10 @@ public class ColorPlatform : MonoBehaviour {
         for (float f = 0; f <= 0.5501; f += fadeSpeed) {
             Color colorCollect = this.GetComponent<Renderer>().material.color;
             // Color colorPlatform = platformDisappear.GetComponent<Renderer>().material.color;
-            if (this.transform.childCount > 0) {
-                light.intensity = 0.55f-f;
-            }
+            light.intensity = 0.55f-f;
             colorCollect.a = 0.55f-f;
             // colorPlatform.a = 0.55f-f;
-            this.GetComponent<Renderer>().material.color = colorCollect;
+            // this.GetComponent<Renderer>().material.color = color;
             yield return new WaitForSecondsRealtime(fadeSpeed);
         }
         thisObject.SetActive(false);
