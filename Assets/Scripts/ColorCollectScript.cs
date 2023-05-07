123aa620fa676e2495bf4321c1c8fb8902231291
Patrick Leary <paleary@davidson.edu>
2023-05-07 17:31:28 -0400

    Update ColorCollectScript.cs
    Fixed the fade out bug

--- Assets/Scripts/ColorCollectScript.cs
+++ Assets/Scripts/ColorCollectScript.cs
@@ -69,10 +69,15 @@ public class ColorCollectScript : MonoBehaviour {
     for (float f = 0; f <= 0.451; f += fadeSpeed) {
         lightAppear.intensity = 0.45f-f;
         colorCollect.a = 0.45f-f;
+        
         foreach (Renderer childRenderer in platformDisappear.GetComponentsInChildren<Renderer>()) {
-            Color childColor = childRenderer.material.color;
-            childColor.a = 0.45f-f;
-            childRenderer.material.color = childColor;
+            Material[] materials = childRenderer.GetComponent<Renderer>().materials;
+            Color color;
+            foreach (Material material in materials) {
+                color = material.color;
+                color.a = 0.45f - f;
+                material.color = color;
+            }
         }
         this.GetComponent<Renderer>().material.color = colorCollect;
         yield return new WaitForSecondsRealtime(fadeSpeed);
